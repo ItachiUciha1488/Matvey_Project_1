598df409f521bdb2c16f60c057ee8249a2d5e16c
Ontoshkaq <freeska2019@mail.ru>
2021-01-21 11:17:52 +0700

    Решение задач

---
+++ src/main.kt
@@ -0,0 +1,60 @@
+//  http://k504.khai.edu/attachments/article/762/Zadachnik_Abramyan.pdf
+
+
+
+fun main() //заглавная функция
+{
+//    var a = 2
+//    var b = 4
+//    var P = 2*(a + b)
+//    var S = a*b
+//    print("Периметр прямоугольника = $P\nПлощадь прямоугольника = $S")
+
+//    var pi = 3.14
+//    print("Введите диаметр: ")
+//    var d = readLine()!!.toDouble()
+//    var L = pi*d
+//    print ("Длина окружности = $L\n ")
+//    print("Введите а: ")
+//    var a = readLine()!!.toDouble()
+//    print("Введите b: ")
+//    var b = readLine()!!.toDouble()
+//    print("Введите c: ")
+//    var c = readLine()!!.toDouble()
+//    var V = a*b*c
+//    var S = 2*(a*b + b*c + a*c)
+//    println("объём прямоугольного параллепипида = $V\nПЛлощадь параллелипипида = $S")
+
+
+
+
+//    print("Введите радиус: ")
+//    var R = readLine()!!.toDouble()
+//
+//    var pi = 3.14
+//    var L = 2*pi*R
+//
+//    var S = pi*R*R
+//    println("Площадь круга = $S\nДлина окружнасти = $L")
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+/*
+fun pencil() //побочная самописная функция - пользовательская
+{
+    sdfa
+    asda
+    asd
+}
+*/
